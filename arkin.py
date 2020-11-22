8023892f8bf6dcf34ff56db8b222be3fa4b4b0cb
Moppit <michelletranusa@gmail.com>
2020-11-22 00:42:39 -0700

    Need to finish Prim's, b-matching, and removing consecutive back edges. Structure is extremely gross right now, please come back and fix this

--- arkin.py
+++ arkin.py
@@ -1,16 +1,161 @@
+import math
+
 class Arkin:
     def __init__(self, pattern):
         self.pattern = pattern
-        self.graph = None
+        self.front_edges = []
+        self.back_edges = []
+        self.t_approx = []
 
-    def make_graph(self):
-        print('make graph if whatever format is easiest. Might add general function to util to just call it')
+    def get_formatted_edge(self, v1, v2):
+        # Ensure uniqueness by ordering coordinates: <x, then <y if x's same
+        if v1[0] < v2[0]:
+            edge = (v1, v2)
+        elif v1[0] > v2[0]:
+            edge = (v2, v1)
+        else:
+            if v1[1] < v2[1]:
+                edge = (v1, v2)
+            elif v1[1] > v2[1]:
+                edge = (v2, v1)
+            else:
+                print('ERROR! Caught a self loop in arkin.py')
+                print(v1, v2)
+                exit(0)
+                edge = ((0, 0), (0, 0))
+        return edge
 
-    def prim(self):
-        print('Do Prim algo to find the MST of the disconnected components')
+    def get_edge_length(self, edge):
+        # Assume a edge is a tuple of 2 tuples ((a,b), (c,d))
+        x_diff = edge[0][0] - edge[1][0]
+        y_diff = edge[0][1] - edge[1][1]
+        return math.sqrt(x_diff**2 + y_diff**2)
 
+    def b_matchings(self):
+        print('TODO: b-matching algorithm for individual components')
+
+    """
+    Finds a connected component within the graph.
+    @param vertex: current node we are evaluating
+    @return component: list of vertices that were part of this component (list)
+    """
+    def dfs(self, vertex, component_so_far, visited):
+        # Grab all neighbors of this vertex
+        component_so_far.append(vertex)
+        adj = [node for node in self.pattern[vertex] if node not in visited]
+        for v in adj:
+            component_so_far = self.dfs(v, component_so_far, visited + [v])
+        return component_so_far
+
+    """
+    Builds G', which is the graph containing all connected components as vertices, and E' which is a complete graph
+    @param: None
+    @return: constructed graph G'(V', E')
+    """
+    def find_components(self):
+        # Find the components with bfs or dfs on each disconnected edge
+        unvisited = list(self.pattern.keys())
+        components = []
+        while unvisited:
+            to_check = unvisited[0]
+            # Do DFS traversal
+            component = self.dfs(to_check, [], [to_check])
+            for node in component:
+                unvisited.remove(node)
+            components.append(component)
+
+        # Create G'
+        # Store each of the the components as vertices
+        g_prime = {}
+        for comp in components:
+            comp.sort()
+            g_prime[tuple(comp)] = []
+        # Create a complete graph on all of those nodes
+        for node_1 in g_prime:
+            for node_2 in g_prime:
+                if node_1 != node_2:
+                    # Now create an edge between them. Make sure they're not the same
+                    min_edge_weight = math.inf
+                    min_edge = None
+                    for v1 in node_1:
+                        for v2 in node_2:
+                            edge = self.get_formatted_edge(v1, v2)
+                            edge_weight = self.get_edge_length(edge)
+                            if edge_weight < min_edge_weight:
+                                min_edge = edge
+                                min_edge_weight = edge_weight
+                    g_prime[node_1].append(min_edge)
+        return g_prime
+
+    """
+    Returns the minimum spanning tree of G'
+    @param: G' (graph)
+    @return: MST of G' (graph)
+    """
+    def prim(self, g_prime):
+        mst_edges = []
+        # Get list of vertices in V' -- keep sorted
+        v_prime = list(g_prime.keys())
+        # Get a list of the edges in E'
+        e_prime = {}
+        queue = []
+        # Choose a starting vertex and add all of its edges to the queue.
+        start = v_prime.pop(0)
+        for edge in g_prime[start]:
+            queue.append((self.get_edge_length(edge), edge))
+        # Pop items off the queue -- if they add something to the tree, add to MST
+        while queue:
+            queue.sort()
+            to_add = queue.pop(0)
+            # Check if one of the endpoints is still a vertex in the graph
+            print('TODO')
+
+        # for vertex in g_prime:
+        #     for edge in g_prime[vertex]:
+        #         dist = self.get_edge_length(edge)
+        #         queue.append((dist, edge))
+        #         if edge not in e_prime:
+        #             e_prime[edge] = []
+        #         e_prime[edge].append(vertex)
+        # # Init list which you will compare to V'
+        # v_included = []
+        # while v_included != v_prime:
+        #     # Take the smallest item off queue
+        #     to_check = queue.pop(0)
+        #     # If one endpoint not covered yet, add to mst edges
+        #     v_included.sort()
+        # Now go through each of the weights from least to greatest until all vertices covered
+        return mst_edges
+
+    """
+    Removes redundant back edges
+    """
     def remove_consecutive(self):
         print('remove consecutive back edges from T_approx')
     
+    """
+    Finds a two approximation for the embroidery problem by the Arkin et al. algorithm.
+    @param: None
+    @return: approximated distance (float)
+    """
     def approx_2(self):
-        print('do the two approx')
+        # Find connected components -- constrct G'(V', E')
+        g_prime = self.find_components()
+        if len(g_prime) == 1:
+            return self.b_matchings()
+        else:
+            # Use Prim's to find the MST
+            mst = self.prim(g_prime)
+            # Find all front edges from given pattern
+            front_edges = []
+            self.front_edges = front_edges
+            # Remove consecutive back edges
+            self.back_edges = mst + mst + front_edges 
+            self.t_approx = self.front_edges + self.back_edges
+            self.remove_consecutive()
+            # Return length of the tour
+            """
+            length = 0
+            for edge in self.t_approx:
+                length += len(edge)
+            """

c220537f88e1e8c469b50a2cdde2705e5c1dc977
Moppit <michelletranusa@gmail.com>
2020-12-12 21:47:53 -0700

    Added timing code and cleaned tsp code

--- main.py
+++ main.py
@@ -26,101 +26,51 @@ pattern = {
 
 # util.visual(m, n, pattern)
 
-# # Timing DPLL
-# num_trials = 100
-# m_upper = 5
-# n_upper = 5
-# # for m in range(3, m_upper+1):
-# #     for n in range(1, n_upper+1):
-# m = 5
-# n = 5
-# # int((m*n)*(m*n-1)/2) is the maximum number of stitches possible
-# for num_stitches in range(1, int((m*n)*(m*n-1)/2)):
-#     for i in range(num_trials):
-#         pattern = util.random_pattern(m, n, num_stitches)
-#         print('Current graph:')
-#         for v in pattern:
-#             print(v, pattern[v])
-#         dpll_obj = dpll.DPLL(pattern)
-#         dpll_obj.make_graph()
-#         num_vertices = len(dpll_obj.pattern)
-#         min_val = math.inf
-#         start_time = time.time()
-#         for vertex in list(dpll_obj.pattern.keys()):
-#             start = vertex
-#             new_val = dpll_obj.dpll(start, start, True, [])
-#             if new_val < min_val:
-#                 min_val = new_val
-#         end_time = time.time()
-#         file_write = open('dpll_stitch_count.csv', 'a')
-#         # Format: m, n, num_stitches, num_vertices, i, time, value
-#         file_write.write(str(m) + ',' + str(n) + ',' + str(num_stitches) + ',' + str(num_vertices) + ',' + str(i) + ',' + str(end_time-start_time) + ',' + str(min_val) + '\n')
-#         file_write.close()
-#         print('Finished trial', i, 'of', num_stitches, 'stitches')
-
-# Testing Arkin
-# approx2_obj = arkin.Arkin(pattern)
-# t_approx_len = approx2_obj.approx_2()
-# print('Arkin 2-approx solution', t_approx_len)
-
-
+"""
+Timing code
+"""
+# Timing DPLL
+num_trials = 100
 m = 5
 n = 5
-num_stitches = 5
-for i in range(10):
-    pattern = util.random_pattern(m, n, num_stitches)
-    print('Current graph:')
-    for v in pattern:
-        print(v, pattern[v])
+# int((m*n)*(m*n-1)/2) is the maximum number of stitches possible
+for num_stitches in range(1, int((m*n)*(m*n-1)/2)):
+    for i in range(num_trials):
+        pattern = util.random_pattern(m, n, num_stitches)
+        num_vertices = len(pattern)
+        print('Current graph:')
+        for v in pattern:
+            print(v, pattern[v])
 
-    # DPLL work
-    dpll_obj = dpll.DPLL(pattern)
-    dpll_obj.make_graph()
-    num_vertices = len(dpll_obj.pattern)
-    min_val = math.inf
-    start_time = time.time()
-    for vertex in list(dpll_obj.pattern.keys()):
-        start = vertex
-        new_val = dpll_obj.dpll(start, start, True, [])
-        if new_val < min_val:
-            min_val = new_val
-    end_time = time.time()
-    dpll_time = end_time - start_time
+        # Create TSP solvers class
+        solver = tsp.TSP_Solver(pattern)
 
-    # Concorde work
-    solver = tsp.TSP_Solver(pattern)
-    start_time = time.time()
-    solver.multi_node_reduction()
-    concorde_val = solver.pyconcorde()
-    end_time = time.time()
-    concorde_time = end_time - start_time
+        # Concorde TSP Solver
+        start_time = time.time()
+        solver.multi_node_reduction()
+        concorde_val = solver.pyconcorde()
+        end_time = time.time()
+        concorde_time = end_time - start_time
 
-    # TSP work -- redo reduction for fairness
-    start_time = time.time()
-    solver.multi_node_reduction()
-    tsp_val = solver.tsp()
-    end_time = time.time()
-    tsp_time = end_time - start_time
+        # Other more accurate but slower TSP Solver (do reduction again for fair comparison)
+        start_time = time.time()
+        solver.multi_node_reduction()
+        tsp_val = solver.tsp()
+        end_time = time.time()
+        tsp_time = end_time - start_time
 
-    # Write to file
-    file_write = open('testing_tsp.csv', 'a')
-    # Format: m, n, num_stitches, num_vertices, i, time, value
-    # file_write.write(str(m) + ',' + str(n) + ',' + str(num_stitches) + ',' + str(num_vertices) + ',' + str(i) + ',' + str(end_time-start_time) + ',' + str(min_val) + '\n')
-    # Format: i, label, distance, time
-    file_write.write(str(i) + ',dpll,' + str(min_val) + ',' + str(dpll_time) + '\n')
-    file_write.write(str(i) + ',conc,' + str(concorde_val) + ',' + str(concorde_time) + '\n')
-    file_write.write(str(i) + ',tsp0,' + str(tsp_val) + ',' + str(tsp_time) + '\n')
-    file_write.close()
-    print('Finished trial', i, 'of', num_stitches, 'stitches')
+        # Write to files
 
+        # Write to Concorde file
+        file_write = open('concorde_stitch_count.csv', 'a')
+        # Format: m, n, num_stitches, num_vertices, i, time, value
+        file_write.write(str(m) + ',' + str(n) + ',' + str(num_stitches) + ',' + str(num_vertices) + ',' + str(i) + ',' + str(concorde_time) + ',' + str(concorde_val) + '\n')
+        file_write.close()
 
+        # Write to other TSP solver file
+        file_write = open('tsp_stitch_count.csv', 'a')
+        # Format: m, n, num_stitches, num_vertices, i, time, value
+        file_write.write(str(m) + ',' + str(n) + ',' + str(num_stitches) + ',' + str(num_vertices) + ',' + str(i) + ',' + str(tsp_time) + ',' + str(tsp_val) + '\n')
+        file_write.close()
 
-
-# # Testing TSP solver
-# solver = tsp.TSP_Solver(pattern)
-# solver.multi_node_reduction()
-# # solver.tsp()
-# solver.pyconcorde()
-# print('=========================================================')
-# solver.tsp()
-# # solver.tsp()
+        print('Finished trial', i, 'of', num_stitches, 'stitches')
--- tsp_solver.py
+++ tsp_solver.py
@@ -4,7 +4,6 @@ import tsp
 import importlib
 util = importlib.import_module('util')
 import math
-# from python_tsp.exact import solve_tsp_dynamic_programming
 
 class TSP_Solver:
     def __init__(self, pattern):
@@ -16,7 +15,7 @@ class TSP_Solver:
     Reduction 1: to undirected TSP.
     Assigned self.symmetric_graph to a 2D adjacency matrix where 9999 means nodes are not connected 
     @param: None
-    @return: None ... TODO: add time taken for reduction
+    @return: None
     """
     def symmetric_reduction(self):
         # Create a map for all original nodes in V
@@ -86,17 +85,9 @@ class TSP_Solver:
     Reduction 2: Modified to accommodate TSP solvers, which only access each vertex once
     Assigned self.multi_node_graph to a 2D adjacency matrix where 9999 means nodes are not connected 
     @param: None
-    @return: None ... TODO: add time taken for reduction
+    @return: None
     """
     def multi_node_reduction(self):
-        # Create all the multiple nodes and devise a map
-        # Need to find some way to track nodes and their neighbors, even with same location
-        # Probably make a list of lists? Or make a map and then 
-        # What if I adjust the pattern? So self.pattern does something now, but what if I modify it?
-        # Or make a copy and do the modifications... so that some locations are there? But how to deal with duplicates?
-        # I think a tuple or something will have to do.... just make a map of (location, num)
-        # Okay, so note: every front node will have a back node. They can be equidistant using the gap, as we expect
-
         # Convert given pattern into set of disjoint edges where each node is duplicated
         # so it can only be paired with one of its neighbors
         expand_pattern = []
@@ -156,38 +147,6 @@ class TSP_Solver:
                 # Add inverse to the visited list
                 gadget_made.append((v2, v1))
 
-        # # Add all front edge gadgets
-        # hashtable = {}
-        # for v in self.pattern:
-        #     adj = self.pattern[v]
-        #     gadget_made = [[],[]]
-        #     if v in hashtable:
-        #         gadget_made = hashtable[v]
-        #     for node in adj:
-        #         # Make distance half of the original
-        #         distance = util.get_edge_length(v, node)/2
-        #         # If there are values in hash table, check if node matches
-        #         if node in gadget_made[0]:
-        #             shared_idx = gadget_made[0].index(node)
-        #             gadget_idx = gadget_made[1][shared_idx]
-        #             front_origin_idx = node_map.index(v)
-        #             # Add existing gadget
-        #             reduction[front_origin_idx].append( (gadget_idx, distance) )
-        #             reduction[gadget_idx].append( (front_origin_idx, distance) )
-        #         # If not in hash table, will have to add other endpoint in hash table
-        #         else:
-        #             # Add gadget to reduction
-        #             new_gadget = len(reduction)
-        #             reduction.append([])
-        #             v_idx = node_map.index(v)
-        #             reduction[new_gadget].append( (v_idx, distance) )
-        #             reduction[v_idx].append( (new_gadget, distance) )
-        #             # Add the endpoint in the hashtable
-        #             if node not in hashtable:
-        #                 hashtable[node] = [[], []]
-        #             hashtable[node][0].append(v)
-        #             hashtable[node][1].append(new_gadget)
-
         # Convert reduction into an adjacency
         self.multi_node_graph = [ [9999 for _ in range(len(reduction))] for _ in range(len(reduction)) ]
         for i in range(len(reduction)):
@@ -195,48 +154,25 @@ class TSP_Solver:
             adj = reduction[i]
             for val in adj:
                 self.multi_node_graph[i][val[0]] = val[1]
-
-        print(reduction)
-        print('------------------------------------------')
-        print(self.multi_node_graph)
-
-
-    """
-    Using the python-tsp Python package by Fillipe Goulart.
-    Dynamic programming approach to solving TSP exactly.
-    """
-    def python_tsp(self):
-        # TODO: For symmetric, make sure to subtract 2|V| at the end!
-        distance_matrix = np.array([
-            [0,  5, 4, 10],
-            [5,  0, 8,  5],
-            [4,  8, 0,  3],
-            [10, 5, 3,  0]
-        ])
-        permutation, distance = solve_tsp_dynamic_programming(distance_matrix)
     
     """
     Using the tsp Python package by Saito Tsutomu.
     URL: https://pypi.org/project/tsp/
-    Linear programming approach to solving TSP exactly many times, but not guaranteed.
+    Linear programming approach to solving TSP with fairly good precision
     """
     def tsp(self):
-        # r = range(len(self.symmetric_graph))
-        r = range(len(self.multi_node_graph))
         # Dictionary of distance
-        # dist = {(i, j): self.symmetric_graph[i][j] for i in r for j in r}
+        r = range(len(self.multi_node_graph))
         dist = {(i, j): self.multi_node_graph[i][j] for i in r for j in r}
         route_length, tour = tsp.tsp(r, dist)
+
         # Calculate excess distance -- every duplicate node needs distance
         excess = 0
         for v in self.pattern:
             excess += 2*len(self.pattern[v])
-        print(route_length - excess)
-        print(tour)
+        
+        # Return length without excess
         return route_length - excess
-        # # TODO: deleteme! Print adj matrix
-        # for row in self.multi_node_graph:
-        #     print([round(n, 2) for n in row])
 
     """
     Using the PyConcorde by Joris Vankerschaver.
@@ -244,21 +180,18 @@ class TSP_Solver:
         created to this day. Uses branch and cut method (linear programming).
     """
     def pyconcorde(self):
-        # TODO: maybe do timing within here, since will have to do some processing
-        # and writing to file stuff first
         # Create tsp file
-        filename = 'test.tsp'
+        filename = 'embroidery.tsp'
         file_write = open(filename, 'w')
-        file_write.write('NAME: test\n')
+        file_write.write('NAME: Embroidery Reduction\n')
         file_write.write('TYPE: TSP\n')
-        file_write.write('COMMENT: testing Concorde on symmetric instance\n')
-        # file_write.write('DIMENSION: ' + str(len(self.symmetric_graph)) + '\n')
+        file_write.write('COMMENT: Concorde on a reduction from the embroidery problem\n')
         file_write.write('DIMENSION: ' + str(len(self.multi_node_graph)) + '\n')
         file_write.write('EDGE_WEIGHT_TYPE: EXPLICIT\n')
         file_write.write('EDGE_WEIGHT_FORMAT: FULL_MATRIX\n')
         file_write.write('EDGE_WEIGHT_SECTION\n')
-        # Go through each row of the matrix, round up value
-        # for row in self.symmetric_graph:
+
+        # Go through each row of the matrix, round up value, write to file
         for row in self.multi_node_graph:
             to_add = ''
             for val in row:
@@ -266,16 +199,14 @@ class TSP_Solver:
             file_write.write(to_add + '\n')
         file_write.write('EOF')
         file_write.close()
-        # print(fname)
+
+        # Solve instances
         solver = TSPSolver.from_tspfile(filename)
         solution = solver.solve()
+
         # Calculate excess distance -- every duplicate node needs distance
         excess = 0
         for v in self.pattern:
             excess += 2*len(self.pattern[v])
-        print('=== found tour? === :', solution.found_tour)
-        print('=== optimal val === :', solution.optimal_value)
-        # print('=== subbed dist === :', solution.optimal_value - 2*len(self.symmetric_graph))
-        print('=== subbed dist === :', solution.optimal_value - excess)
 
         return solution.optimal_value - excess

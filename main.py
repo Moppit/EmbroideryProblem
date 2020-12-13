ae0a9b87c0a8447b9e2e02bc9b47cc86e1956b4f
Moppit <michelletranusa@gmail.com>
2020-12-12 21:11:36 -0700

    Finished TSP section :D need to clean up

--- main.py
+++ main.py
@@ -63,9 +63,64 @@ pattern = {
 # t_approx_len = approx2_obj.approx_2()
 # print('Arkin 2-approx solution', t_approx_len)
 
-# Testing TSP solver
-solver = tsp.TSP_Solver(pattern)
-solver.symmetric_reduction()
+
+m = 5
+n = 5
+num_stitches = 5
+for i in range(10):
+    pattern = util.random_pattern(m, n, num_stitches)
+    print('Current graph:')
+    for v in pattern:
+        print(v, pattern[v])
+
+    # DPLL work
+    dpll_obj = dpll.DPLL(pattern)
+    dpll_obj.make_graph()
+    num_vertices = len(dpll_obj.pattern)
+    min_val = math.inf
+    start_time = time.time()
+    for vertex in list(dpll_obj.pattern.keys()):
+        start = vertex
+        new_val = dpll_obj.dpll(start, start, True, [])
+        if new_val < min_val:
+            min_val = new_val
+    end_time = time.time()
+    dpll_time = end_time - start_time
+
+    # Concorde work
+    solver = tsp.TSP_Solver(pattern)
+    start_time = time.time()
+    solver.multi_node_reduction()
+    concorde_val = solver.pyconcorde()
+    end_time = time.time()
+    concorde_time = end_time - start_time
+
+    # TSP work -- redo reduction for fairness
+    start_time = time.time()
+    solver.multi_node_reduction()
+    tsp_val = solver.tsp()
+    end_time = time.time()
+    tsp_time = end_time - start_time
+
+    # Write to file
+    file_write = open('testing_tsp.csv', 'a')
+    # Format: m, n, num_stitches, num_vertices, i, time, value
+    # file_write.write(str(m) + ',' + str(n) + ',' + str(num_stitches) + ',' + str(num_vertices) + ',' + str(i) + ',' + str(end_time-start_time) + ',' + str(min_val) + '\n')
+    # Format: i, label, distance, time
+    file_write.write(str(i) + ',dpll,' + str(min_val) + ',' + str(dpll_time) + '\n')
+    file_write.write(str(i) + ',conc,' + str(concorde_val) + ',' + str(concorde_time) + '\n')
+    file_write.write(str(i) + ',tsp0,' + str(tsp_val) + ',' + str(tsp_time) + '\n')
+    file_write.close()
+    print('Finished trial', i, 'of', num_stitches, 'stitches')
+
+
+
+
+# # Testing TSP solver
+# solver = tsp.TSP_Solver(pattern)
+# solver.multi_node_reduction()
+# # solver.tsp()
+# solver.pyconcorde()
+# print('=========================================================')
 # solver.tsp()
-solver.pyconcorde()
-# solver.tsp()
+# # solver.tsp()
--- tsp_solver.py
+++ tsp_solver.py
@@ -10,7 +10,7 @@ class TSP_Solver:
     def __init__(self, pattern):
         self.pattern = pattern
         self.symmetric_graph = None
-        self.asymmetric_graph = None
+        self.multi_node_graph = None
 
     """
     Reduction 1: to undirected TSP.
@@ -75,20 +75,131 @@ class TSP_Solver:
                     hashtable[node][1].append(new_gadget)
 
         # Convert reduction into an adjacency
-        self.symmetric_graph = [ [1000 for _ in range(len(reduction))] for _ in range(len(reduction)) ]
+        self.symmetric_graph = [ [9999 for _ in range(len(reduction))] for _ in range(len(reduction)) ]
         for i in range(len(reduction)):
             self.symmetric_graph[i][i] = 0
             adj = reduction[i]
             for val in adj:
                 self.symmetric_graph[i][val[0]] = val[1]
 
-    def asymmetric_reduction(self):
-        print('Assymetric reduction')
-        # Create adj. matrix (not numpy array... can do conversion within python-tsp call)
-        # What does the adj matrix look like?
-        # - Self loops = 0
-        # - Non-connected nodes = 9999
-        # - Symmetric reduction
+    """
+    Reduction 2: Modified to accommodate TSP solvers, which only access each vertex once
+    Assigned self.multi_node_graph to a 2D adjacency matrix where 9999 means nodes are not connected 
+    @param: None
+    @return: None ... TODO: add time taken for reduction
+    """
+    def multi_node_reduction(self):
+        # Create all the multiple nodes and devise a map
+        # Need to find some way to track nodes and their neighbors, even with same location
+        # Probably make a list of lists? Or make a map and then 
+        # What if I adjust the pattern? So self.pattern does something now, but what if I modify it?
+        # Or make a copy and do the modifications... so that some locations are there? But how to deal with duplicates?
+        # I think a tuple or something will have to do.... just make a map of (location, num)
+        # Okay, so note: every front node will have a back node. They can be equidistant using the gap, as we expect
+
+        # Convert given pattern into set of disjoint edges where each node is duplicated
+        # so it can only be paired with one of its neighbors
+        expand_pattern = []
+        for node in self.pattern:
+            for neighbor in self.pattern[node]:
+                expand_pattern.append( (node, neighbor) )
+
+        # Create a map for all of the duplicated nodes from the original graph
+        # Treat every node as its first constituent -- use other node for identification
+        node_map = [node for node in expand_pattern]
+        gap = len(node_map)
+
+        # Create empty lists for all v in V, v' in V, and alternating gadgets
+        reduction = [ [] for _ in range(3*gap)]
+
+        # Add all neighbors to v' nodes and alternating gadget edges
+        for i in range(gap, 2*gap):
+            # Add all alternating gadget edges (both directions, both fabric sides)
+            # V' <-> alternating gadget
+            reduction[i].append( (i+gap, 1) )
+            reduction[i+gap].append( (i, 1) )
+            # V <-> alternating gadget
+            reduction[i-gap].append( (i+gap, 1) )
+            reduction[i+gap].append( (i-gap, 1) )
+            # Add all other vertices if not the corresponding v vertex for v'
+            # (or the same first node)
+            for node in node_map:
+                node_idx = node_map.index(node)
+                # Make sure they don't have the same first node (could also check this by dist != 0)
+                if node_idx != i-gap and node[0] != node_map[i-gap][0]:
+                    dist = util.get_edge_length(node[0], node_map[i-gap][0])
+                    reduction[i].append( (node_idx+gap, dist) )
+
+        # Add all front edge gadgets
+        gadget_made = []
+        for pair in node_map:
+            v1 = pair[0]
+            v2 = pair[1]
+            # See if we made the gadget already -- if so, skip
+            if pair in gadget_made:
+                gadget_made.remove(pair)
+            # If not, create gadget for both sides
+            else:
+                # Make distance half of the original
+                distance = util.get_edge_length(pair[0], pair[1])/2
+
+                # Add gadget to reduction
+                new_gadget = len(reduction)
+                reduction.append([])
+                pair_fwd_idx = node_map.index(pair)
+                pair_backward_idx = node_map.index( (v2, v1) )
+                reduction[new_gadget].append( (pair_fwd_idx, distance) )
+                reduction[new_gadget].append( (pair_backward_idx, distance) )
+                reduction[pair_fwd_idx].append( (new_gadget, distance) )
+                reduction[pair_backward_idx].append( (new_gadget, distance) )
+                
+                # Add inverse to the visited list
+                gadget_made.append((v2, v1))
+
+        # # Add all front edge gadgets
+        # hashtable = {}
+        # for v in self.pattern:
+        #     adj = self.pattern[v]
+        #     gadget_made = [[],[]]
+        #     if v in hashtable:
+        #         gadget_made = hashtable[v]
+        #     for node in adj:
+        #         # Make distance half of the original
+        #         distance = util.get_edge_length(v, node)/2
+        #         # If there are values in hash table, check if node matches
+        #         if node in gadget_made[0]:
+        #             shared_idx = gadget_made[0].index(node)
+        #             gadget_idx = gadget_made[1][shared_idx]
+        #             front_origin_idx = node_map.index(v)
+        #             # Add existing gadget
+        #             reduction[front_origin_idx].append( (gadget_idx, distance) )
+        #             reduction[gadget_idx].append( (front_origin_idx, distance) )
+        #         # If not in hash table, will have to add other endpoint in hash table
+        #         else:
+        #             # Add gadget to reduction
+        #             new_gadget = len(reduction)
+        #             reduction.append([])
+        #             v_idx = node_map.index(v)
+        #             reduction[new_gadget].append( (v_idx, distance) )
+        #             reduction[v_idx].append( (new_gadget, distance) )
+        #             # Add the endpoint in the hashtable
+        #             if node not in hashtable:
+        #                 hashtable[node] = [[], []]
+        #             hashtable[node][0].append(v)
+        #             hashtable[node][1].append(new_gadget)
+
+        # Convert reduction into an adjacency
+        self.multi_node_graph = [ [9999 for _ in range(len(reduction))] for _ in range(len(reduction)) ]
+        for i in range(len(reduction)):
+            self.multi_node_graph[i][i] = 0
+            adj = reduction[i]
+            for val in adj:
+                self.multi_node_graph[i][val[0]] = val[1]
+
+        print(reduction)
+        print('------------------------------------------')
+        print(self.multi_node_graph)
+
 
     """
     Using the python-tsp Python package by Fillipe Goulart.
@@ -110,15 +221,22 @@ class TSP_Solver:
     Linear programming approach to solving TSP exactly many times, but not guaranteed.
     """
     def tsp(self):
-        r = range(len(self.symmetric_graph))
+        # r = range(len(self.symmetric_graph))
+        r = range(len(self.multi_node_graph))
         # Dictionary of distance
-        dist = {(i, j): self.symmetric_graph[i][j] for i in r for j in r}
+        # dist = {(i, j): self.symmetric_graph[i][j] for i in r for j in r}
+        dist = {(i, j): self.multi_node_graph[i][j] for i in r for j in r}
         route_length, tour = tsp.tsp(r, dist)
-        print(route_length - 2*len(self.pattern))
+        # Calculate excess distance -- every duplicate node needs distance
+        excess = 0
+        for v in self.pattern:
+            excess += 2*len(self.pattern[v])
+        print(route_length - excess)
         print(tour)
-        # TODO: deleteme! Print adj matrix
-        for row in self.symmetric_graph:
-            print([round(n, 2) for n in row])
+        return route_length - excess
+        # # TODO: deleteme! Print adj matrix
+        # for row in self.multi_node_graph:
+        #     print([round(n, 2) for n in row])
 
     """
     Using the PyConcorde by Joris Vankerschaver.
@@ -134,21 +252,30 @@ class TSP_Solver:
         file_write.write('NAME: test\n')
         file_write.write('TYPE: TSP\n')
         file_write.write('COMMENT: testing Concorde on symmetric instance\n')
-        file_write.write('DIMENSION: ' + str(len(self.symmetric_graph)) + '\n')
+        # file_write.write('DIMENSION: ' + str(len(self.symmetric_graph)) + '\n')
+        file_write.write('DIMENSION: ' + str(len(self.multi_node_graph)) + '\n')
         file_write.write('EDGE_WEIGHT_TYPE: EXPLICIT\n')
         file_write.write('EDGE_WEIGHT_FORMAT: FULL_MATRIX\n')
         file_write.write('EDGE_WEIGHT_SECTION\n')
         # Go through each row of the matrix, round up value
-        for row in self.symmetric_graph:
+        # for row in self.symmetric_graph:
+        for row in self.multi_node_graph:
             to_add = ''
             for val in row:
                 to_add += str(math.ceil(val)) + ' '
-            file_write.write(to_add)
+            file_write.write(to_add + '\n')
         file_write.write('EOF')
         file_write.close()
         # print(fname)
         solver = TSPSolver.from_tspfile(filename)
         solution = solver.solve()
+        # Calculate excess distance -- every duplicate node needs distance
+        excess = 0
+        for v in self.pattern:
+            excess += 2*len(self.pattern[v])
         print('=== found tour? === :', solution.found_tour)
         print('=== optimal val === :', solution.optimal_value)
-        print('=== subbed dist === :', solution.optimal_value - 2*len(self.symmetric_graph))
+        # print('=== subbed dist === :', solution.optimal_value - 2*len(self.symmetric_graph))
+        print('=== subbed dist === :', solution.optimal_value - excess)
+
+        return solution.optimal_value - excess

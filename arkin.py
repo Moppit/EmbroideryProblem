9d5433af3d8686944fc2ef6498810c34126f9c42
Moppit <michelletranusa@gmail.com>
2020-12-14 00:59:45 -0700

    Added connected component algo -- used approx instead of b-matching

--- arkin.py
+++ arkin.py
@@ -1,12 +1,20 @@
 import math
+import time
 
 class Arkin:
     def __init__(self, pattern):
         self.pattern = pattern
+        self.back_matrix = None
         self.front_edges = []
         self.back_edges = []
         self.t_approx = []
 
+    """
+    Helper function: ensures all edges are formatted consistently
+    @param v1: an edge endpoint (vertex)
+    @param v2: the other edge endpoint (vertex)
+    @return: edge, formatted as (vi, vj) where vi and vj are coordinates
+    """
     def get_formatted_edge(self, v1, v2):
         # Ensure uniqueness by ordering coordinates: <x, then <y if x's same
         if v1[0] < v2[0]:
@@ -25,6 +33,11 @@ class Arkin:
                 edge = ((0, 0), (0, 0))
         return edge
 
+    """
+    Helper function: calculates edge weight
+    @param edge: edge, formatted as (vi, vj) where vi and vj are coordinates
+    @return: distance, which is the weight of the edge
+    """
     def get_edge_length(self, edge):
         # Assume a edge is a tuple of 2 tuples ((a,b), (c,d))
         x_diff = edge[0][0] - edge[1][0]
@@ -32,17 +45,73 @@ class Arkin:
         return math.sqrt(x_diff**2 + y_diff**2)
 
     """
-    For connected instances of G, we can us a polynomial algorithm
+    Helper function: formats pattern as an adjacency matrix, for b-matching algo
+    Note: this gets an adjacency matrix of the back edges _only_
+    @param b_assign: list denoting locations and their b value -> [((i,j), a), ((x,y), b)]
+    @return: None
+    """
+    def get_back_matrix(self, b_assign):
+        # Create |b_assign| x |b_assign| filled with 9999 if not connected
+        self.back_matrix = [ [9999 for _ in range(len(b_assign))] for _ in range(len(b_assign)) ]
+        for i in range(len(b_assign)):
+            for j in range(len(b_assign)):
+                if i == j:
+                    self.back_matrix[i][j] = 0
+                else:
+                    self.back_matrix[i][j] = self.get_edge_length((b_assign[i][0], b_assign[j][0]))
+
+    """
+    For connected instances of G, it is the b-matching problem, which has a polynomial algorithm.
+    Uses PuLP linear programming package by J.S. Roy and S.A. Mitchell.
+    Link: https://pypi.org/project/PuLP/
     @param: None
     @return dist: optimal tour length (float)
+    TODO: future implementation
     """
     def b_matchings(self):
         # Idea: we want to ensure that #back edges = #front edges for all vertices
         # "Stage 1. A minimum cost flow problem and its dual are solved (a Hitchcock transposition problem)."
+        # Get b mapping and back adjacency matrix
+        b_assign = []
+        for v in self.pattern:
+            num_neighbors = len(self.pattern[v])
+            b_assign.append((v, num_neighbors))
+        print(b_assign)
+        self.get_back_matrix(b_assign)
+        
+        # Using b mapping to add b value constraints
+        # Us PuLP solver
+        """
+        How to add constraints on variables:
+        Use LpVariable() to create new variables. To create a variable 0 <= x <= 3:
+        x = LpVariable("x", 0, 3)
+        To create a variable 0 <= y <= 1:
+        y = LpVariable("y", 0, 1)
+
+        Create the problem
+        Use LpProblem() to create new problems. Create “myProblem”:
+        prob = LpProblem("myProblem", LpMinimize)
+
+        Add general problem constraints like this:
+        prob += x + y <= 2
+
+        If the thing you add isn't a constraint, it becomes the objective
+        prob += -4*x + y
+
+        Solve with default solver:
+        status = prob.solve()
+
+        Display status
+        LpStatus[status]
+        > 'Optimal'
+
+        Get values of each variable:
+        value(x)
+        > 2.0
+        """
         # "Stage 2. The solutions are processed to obtain optimal 'symmetric' half-integral solutions and 
         #           then to obtain nearly optimal integral solutions."
         # "Stage 3. The nearly optimal solutions are used as input to Pulleyblank's b-matching algorithm."
-
         # So, we do the min b-matching problem on the back graph with vertex weights b(v) = deg_f(v)
         # Algo in paper: looks like it wants a min b-matching, whereas paper wants max b-matching
         # Then return the size of B edges + size of E edges
@@ -67,7 +136,7 @@ class Arkin:
     @return: constructed graph G'(V', E')
     """
     def find_components(self):
-        # Find the components with bfs or dfs on each disconnected edge
+        # Find the components with dfs on each disconnected edge
         unvisited = list(self.pattern.keys())
         components = []
         while unvisited:
@@ -140,6 +209,8 @@ class Arkin:
     
     """
     Finds a two approximation for the embroidery problem by the Arkin et al. algorithm.
+    Note: Arkin et al cite Anstee's b-matching algorithm for connected instances, however, this
+          implementation uses a 2-approximation for connected components as well
     @param: None
     @return: approximated distance (float)
     """
@@ -147,7 +218,17 @@ class Arkin:
         # Find connected components -- constrct G'(V', E')
         g_prime = self.find_components()
         if len(g_prime) == 1:
-            return self.b_matchings()
+            # return self.b_matchings() # TODO: implement this in the future
+            # 2 approximation -- same distance as front edges
+            already_counted = []
+            total_len = 0
+            for v1 in self.pattern:
+                for v2 in self.pattern[v1]:
+                    edge = self.get_formatted_edge(v1, v2)
+                    if edge not in already_counted:
+                        already_counted.append(edge)
+                        total_len += self.get_edge_length(edge)
+            return total_len*2
         else:
             # Use Prim's to find the MST
             mst = self.prim(g_prime)
